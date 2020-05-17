84dda450ee810987ff24f5d6b8a160af6d31512d
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2020-05-17 16:38:02 +0700

    Pembaruan : Sepatu Penunjuk Arah Menghapus line dengan rumus yang mirip agar lebih efisien

--- Struktur Data/Final/Sepatu Penunjuk Arah.cpp
+++ Struktur Data/Final/Sepatu Penunjuk Arah.cpp
@@ -5,23 +5,12 @@ typedef long long ll;
 int main() {
     ll n;
     cin>>n;
-    if(n & 1) {
-        ll k = (n - 1)/2, res = 1 + (2*k * (k + 1));
-        ll tmp = res / 10, res2 = tmp + res;
-        while(res != res2) {
-            ll temp = (res2/10 - res/10);
-            res = res2;
-            res2 += temp;
-        }
-        cout<<res2<<'\n';
-    } else {
-        ll k = (n - 2)/2, res = 2 + (2*k * (k + 2));
-        ll tmp = res / 10, res2 = tmp + res;
-        while(res != res2) {
-            ll temp = (res2/10 - res/10);
-            res = res2;
-            res2 += temp;
-        }
-        cout<<res2<<'\n';
+    ll k = (n - 1)/2, fak = ((n & 1) ? 1 : 2), res = fak + (2*k * (k + fak));
+    ll tmp = res / 10, res2 = tmp + res;
+    while(res != res2) {
+        ll temp = (res2/10 - res/10);
+        res = res2;
+        res2 += temp;
     }
+    cout<<res2<<'\n';
 }
